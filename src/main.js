0f9e26b5325af982741fea1903e5529402bfa6dc
Pascal H <p.harprecht@gmail.com>
2020-02-29 17:57:12 +0100

    keeping track of already checked urls and setting icon accordingly

--- src/main.js
+++ src/main.js
@@ -1,3 +1,5 @@
+var tabState = []
+
 browser.browserAction.onClicked.addListener( function(tab) {
 
 	var query = { active: true, currentWindow: true };
@@ -7,7 +9,7 @@ browser.browserAction.onClicked.addListener( function(tab) {
 		console.log(currTab.url);
 		browser.runtime.sendNativeMessage
 			("mpvff",
-			 { url: currTab.url,request: "play" }
+			 { url: currTab.url,request: "play", id: currTab.id }
 			).then(console.log("resolved"));
 	}
 
@@ -17,20 +19,49 @@ browser.browserAction.onClicked.addListener( function(tab) {
 
 browser.tabs.onActivated.addListener( async function(tab) {
 
-	try {
-		let tabInfo = await browser.tabs.get(tab.tabId);
+	async function checkPlayability(url, tabId){
+		try{
 		let response = await browser.runtime.sendNativeMessage
 			("mpvff",
-			 { url: tabInfo.url, request: "check" }
+			 { url: url, request: "check", id: tabId }
 			);
+		console.log(response)
 		if( response.successful === true) {
 			console.log("Playing available")
+			return true
 		} else {
 			console.log("Playing not available")
+			return false
+		}
+		} catch (error) {
+			console.error(error)
+			return false
 		}
 	}
-	catch (error) {
-		console.error(error);
+
+	if(!tabState[tab.tabId]) {
+		console.log("Tab not yet in table")
+		tabState[tab.tabId] = {
+			id: tab.tabId,
+			url: null,
+			playable: false
+		}
+	}
+
+	let tabInfo = await browser.tabs.get(tab.tabId)
+
+	if(tabState[tab.tabId].url !== tabInfo.url) {
+		console.log("Checking for mpv/youtube-dl compatibility")
+		tabState[tab.tabId].url = tabInfo.url
+		tabState[tab.tabId].playable = await checkPlayability(tabInfo.url, tabInfo.id)
+	}
+
+	if(tabState[tab.tabId].playable === true){
+		console.log("Setting icon to playable")
+		browser.browserAction.setIcon({path:"icons/play.png"})
+	} else {
+		console.log("Setting icon to not playable")
+		browser.browserAction.setIcon({path:"icons/icon.png"})
 	}
 }
 );
